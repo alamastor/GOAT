dfef261f723410da12c1274a4a219a8a970f4570
Alistair McKelvie <alistair.mckelvie@gmail.com>
2016-03-16 21:11:37 +1100

    feat: Working item duplication validation

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -7,7 +7,10 @@ from unittest import skip
 
 from lists.views import home_page
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ItemForm, ExistingListItemForm
+)
 
 
 class HomePageTest(TestCase):
@@ -97,7 +100,7 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
@@ -106,10 +109,9 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -117,10 +119,9 @@ class ListViewTest(TestCase):
             '/lists/%d/' % (list1.id,),
             data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
-        self.assertContains(response, expected_error)
+        self.assertContains(response, escape(DUPLICATE_ITEM_ERROR))
         self.assertTemplateUsed(response, 'list.html')
-        self.assetEqual(Item.objects.all().count(), 1)
+        self.assertEqual(Item.objects.all().count(), 1)
 
 
 class NewListTest(TestCase):
